cf0c8b7e4eb1e3c3d4065c47c3cba37531ccdde6
PenSeeYou <48435089+PenSeeYou@users.noreply.github.com>
2020-06-20 01:30:13 +0800

    Update index.js

--- nodejsService/index.js
+++ nodejsService/index.js
@@ -16,14 +16,114 @@ connection.connect();
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     var params = url.parse(req.url, true).pathname;
-    if (params == "/runSQL") {
+    if (params == "/selectSQL") {
         var params = url.parse(req.url, true).query;
         var sql = params.sql;
         // 查
         connection.query(sql, function (err, result) {
             if (err) {
                 console.log('[SELECT ERROR] - ', err.message);
-                res.end("SQL运行错误！")
+                res.end("SQL语句错误！");
+                return;
+            }
+            var str = JSON.stringify(result);
+            res.end(str);
+        });
+    } else if (params == "/loginSign") {
+        var params = url.parse(req.url, true).query;
+        var user = params.user;
+        // 查询用户密码
+        // 使用用户名查询密码
+        connection.query("SELECT * FROM UserTable WHERE userName='" + user + "'", function (err, result) {
+            if (err) {
+                console.log('[SELECT ERROR] - ', err.message);
+                res.end("SQL语句错误！");
+                return;
+            }
+            var str = JSON.stringify(result);
+            res.end(str);
+        });
+    } else if (params == "/SearchSQL") {
+        var params = url.parse(req.url, true).query;
+        var messagewhat = params.messagewhat;
+        var linkID = params.linkid;
+        var MysqlID = params.MysqlID;
+        var FirstNa = params.FirstNa;
+        
+        // 查询
+
+        if (messagewhat == "0") {
+            connection.query("SELECT * FROM WarringA WHERE LinkID='" + linkID + "'", function (err, result) {
+                if (err) {
+                    console.log('[SELECT ERROR] - ', err.message);
+                    res.end("SQL语句错误！");
+                    return;
+                }
+                var str = JSON.stringify(result);
+                res.end(str);
+            });
+        } else if (messagewhat == "1") {
+            connection.query("SELECT * FROM WarningTotal WHERE LinkID='" + linkID + "'", function (err, result) {
+                if (err) {
+                    console.log('[SELECT ERROR] - ', err.message);
+                    res.end("SQL语句错误！");
+                    return;
+                }
+                var str = JSON.stringify(result);
+                res.end(str);
+            });
+            
+        } else if (messagewhat == "2") {
+            connection.query("SELECT * FROM WarringA WHERE id=" + MysqlID, function (err, result) {
+                if (err) {
+                    console.log('[SELECT ERROR] - ', err.message);
+                    res.end("SQL语句错误！");
+                    return;
+                }
+                var str = JSON.stringify(result);
+                res.end(str);
+            });
+        }
+        else if (messagewhat == "3") {
+            connection.query("SELECT * FROM WarringA WHERE LinkID='"+ linkID +"' and WarringStudent LIKE '%"+ FirstNa +"%'", function (err, result) {
+                if (err) {
+                    console.log('[SELECT ERROR] - ', err.message);
+                    res.write(FirstNa);
+                    res.end("SQL语句错误！");
+                    return;
+                }
+                var str = JSON.stringify(result);
+                res.end(str);
+            });
+        }
+        
+    } else if (params == "/AddWarning") {
+        var params = url.parse(req.url, true).query;
+        var Title = params.Title;
+        var WarningGroup = params.WarningGroup;
+        var WarningStudent = params.WarningStudent;
+        var WarningFun = params.WarningFun;
+        var FunStartTime = params.FunStartTime;
+        var FunEndTime = params.FunEndTime;
+        var BeizhuSS = params.BeizhuSS;
+        var ee = params.ee;
+        var LinkID = params.linkid;
+        var Total = params.Total;
+
+        connection.query("INSERT INTO WarringA  VALUES (null,'" + Title + "','" + WarningGroup + "','" + WarningStudent + 
+        "','"+ WarningFun + "','" + FunStartTime + "','" + FunEndTime +"','" + BeizhuSS +"','" + ee +"','" + LinkID +"')", function (err, result) {
+            if (err) {
+                console.log('[SELECT ERROR] - ', err.message);
+                res.end("SQL语句错误！");
+                return;
+            }
+            var str = JSON.stringify(result);
+            res.end(str);
+        });
+        connection.query("UPDATE WarningTotal SET WarningTotal="+ Total +" WHERE LinkID='" + LinkID + "'", function (err, result) {
+            if (err) {
+                console.log('[SELECT ERROR] - ', err.message);
+                res.end("SQL语句错误！");
                 return;
             }
             var str = JSON.stringify(result);
